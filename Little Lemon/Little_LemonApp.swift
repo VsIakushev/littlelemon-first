72ccaaedf77037e872d590bf07053b247d73dc16
Vitalii Iakushev <vsiakushev@gmail.com>
2023-01-28 17:31:18 +0400

    Initial Commit

---
+++ Little Lemon/Little_LemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  Little_LemonApp.swift
+//  Little Lemon
+//
+//  Created by Vitaliy Iakushev on 28.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct Little_LemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

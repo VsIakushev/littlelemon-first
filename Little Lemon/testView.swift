dd3dde9af8d8a33683ad01b2ef13e54aabb9bb96
Vitalii Iakushev <vsiakushev@gmail.com>
2023-01-28 17:46:10 +0400

    First Little Lemon commit via terminal

---
+++ Little Lemon/testView.swift
@@ -0,0 +1,20 @@
+//
+//  testView.swift
+//  Little Lemon
+//
+//  Created by Vitaliy Iakushev on 28.01.2023.
+//
+
+import SwiftUI
+
+struct testView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct testView_Previews: PreviewProvider {
+    static var previews: some View {
+        testView()
+    }
+}
